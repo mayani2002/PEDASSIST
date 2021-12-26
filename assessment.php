f55c7a8eddef8ddece87a5ef91db013dcdae131b
mayani <arti.agni01@gmail.com>
2021-12-26 23:56:33 +0530

    responsive assessment done

--- assessment.php
+++ assessment.php
@@ -1,4 +1,4 @@
-<?php define('allow',true); ?>
+<?php define('allow', true); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -37,37 +37,45 @@
         <!-- LESSON TRACK -->
         <div class="track">
             <div class="lesson_1 animate__animated animate__fadeInUp">
-                <h2 onclick="document.location.href='questions.php?lesson=1'">LESSON ONE</h2>
-                <div class="indicator_lesson_1"></div>
+                <div class="mcq_btn mcq_btn_l" onclick="document.location.href='questions.php?lesson=1'">
+                    <h2>LESSON ONE</h2>
+                    <div class="indicator_lesson_1"></div>
+                </div>
                 <svg width="213" height="86" viewBox="0 0 213 86" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 1C51.8857 93.8924 161.992 114.793 212 38.157" stroke="black" stroke-width="2" stroke-linecap="round"/>
+                    <path d="M1 1C51.8857 93.8924 161.992 114.793 212 38.157" stroke="black" stroke-width="2" stroke-linecap="round" />
                 </svg>
             </div>
+
             <div class="lesson_2 animate__animated animate__fadeInDown">
                 <svg width="212" height="76" viewBox="0 0 212 76" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 75C51.589 -13.8856 163.552 -17.4848 211 36.5038" stroke="black" stroke-width="2" stroke-linecap="round"/>
+                    <path d="M1 75C51.589 -13.8856 163.552 -17.4848 211 36.5038" stroke="black" stroke-width="2" stroke-linecap="round" />
                 </svg>
-                <div class="indicator_lesson_2"></div>
-                <h2 onclick="document.location.href='questions.php?lesson=2'">LESSON TWO</h2>
+                <div class="mcq_btn mcq_btn_r" onclick="document.location.href='questions.php?lesson=2'">
+                    <div class="indicator_lesson_2"></div>
+                    <h2>LESSON TWO</h2>
+                </div>
             </div>
             <div class="lesson_3 animate__animated animate__fadeInUp">
-                <h2 onclick="document.location.href='questions.php?lesson=3'">LESSON THREE</h2>
-                <div class="indicator_lesson_3"></div>
+                <div class="mcq_btn mcq_btn_l" onclick="document.location.href='questions.php?lesson=3'">
+                    <h2>LESSON THREE</h2>
+                    <div class="indicator_lesson_3"></div>
+                </div>
                 <svg width="213" height="86" viewBox="0 0 213 86" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 1C51.8857 93.8924 161.992 114.793 212 38.157" stroke="black" stroke-width="2" stroke-linecap="round"/>
-                </svg>    
+                    <path d="M1 1C51.8857 93.8924 161.992 114.793 212 38.157" stroke="black" stroke-width="2" stroke-linecap="round" />
+                </svg>
             </div>
             <div class="lesson_4 animate__animated animate__fadeInDown">
                 <svg width="212" height="76" viewBox="0 0 212 76" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 75C51.589 -13.8856 163.552 -17.4848 211 36.5038" stroke="black" stroke-width="2" stroke-linecap="round"/>
+                    <path d="M1 75C51.589 -13.8856 163.552 -17.4848 211 36.5038" stroke="black" stroke-width="2" stroke-linecap="round" />
                 </svg>
-                <div class="indicator_lesson_4"></div>
-                <h2 onclick="document.location.href='questions.php?lesson=4'">LESSON FOUR</h2>                    
+                <div class="mcq_btn mcq_btn_r" onclick="document.location.href='questions.php?lesson=4'">
+                    <div class="indicator_lesson_4"></div>
+                    <h2>LESSON FOUR</h2>
+                </div>
             </div>
             <div class="congrats animate__animated animate__fadeInUp">
-                <div class="indicator_congrats">
-                    <img src="ASSESMENT/giftbox.png" alt="">
-                </div>
+                <div class="indicator_congrats"> </div>
+                <img src="ASSESMENT/giftbox.png" alt="">
             </div>
         </div>
     </main>
@@ -75,10 +83,11 @@
     <script type="text/javascript" src="assets/basic.js"></script>
     <script type="text/javascript" src="login.js"></script>
     <script>
-        function sendCookieInfo(){
+        function sendCookieInfo() {
             var cookie_info = "<?php echo $cookie ?>";
             hideLoginButton(cookie_info);
-            displayLoginSignupForm(cookie_info);
+            // displayLoginSignupFormWithDelay(cookie_info);
+            // signUpOrLoginToContinue(cookie_info);
         }
         sendCookieInfo();
     </script>
